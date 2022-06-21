3b407912ca58124fcb7d10636c44d08fa84a07c9
Joa705 <jpalbertse@hotmail.com>
2022-06-21 15:45:24 +0200

    added

--- js/create.js
+++ js/create.js
@@ -1,4 +1,67 @@
 
-function create(){
-    console.log("test123");
+async function create_penn(){
+    // Get user input
+    var name = document.getElementById('name').value;
+    var type = document.getElementById('type').value;
+    var color = document.getElementById('color').value;
+    var firma_id = document.getElementById('firma_id').value;
+
+    // Check for empty input
+    if(name == ""){name = null};
+    if(type == ""){type = null};
+    if(color == ""){color = null};
+    if(firma_id == ""){console.log("Field firma_id must be filled out"); return;};
+
+    // Get image
+    const image_temp_file = document.getElementById('image');
+    const image = image_temp_file.files[0];
+
+    // Convert to form data
+    const formdata = new FormData();
+    formdata.append('name', name);
+    formdata.append("type", type);
+    formdata.append("color", color);
+    formdata.append("firma_id", firma_id);
+    formdata.append("image", image);
+
+    
+    console.log(name);
+    console.log(type);
+    console.log(color);
+    console.log(firma_id);
+    console.log(image);
+
+    // Send post request
+    send_request(formdata, "http://localhost/Penner/api/penn/create.php")
+    .then(data => {
+        const status = document.getElementById('status');
+        const form_result = document.createElement('div');
+        form_result.appendChild(document.createTextNode(data.message));
+        status.append(form_result);
+    })
+    .catch(error => console.log(error))
+}
+
+// Send create request to the api server
+async function send_request(formdata, url){
+    const login = 'Svein';
+    const password = '12345';
+    const response = await fetch(url, {
+    method: 'POST',
+    headers:{
+       "Authorization": `Basic ${btoa(`${login}:${password}`)}`,
+
+    },
+    body: formdata
+    })
+
+    return response.json();
+}
+
+
+// Display input image 
+function load_image(event) {
+    var image = document.getElementById('img-output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+    
 }
--- js/read.js
+++ js/read.js
@@ -1,51 +1,105 @@
+url = 'localhost'
 
-
-// Fetch url penns
-async function fetch_penns() {
-   const url ="http://localhost/Penner/api/penn/read.php";
-   const login = 'Svein';
-   const password = '12345';
-   const response = await fetch(url, {
-   headers:{
-      "Authorization": `Basic ${btoa(`${login}:${password}`)}`,
-      'Content-Type': 'application/json'
-   }
-   });
-
-   return response.json();
-  
+// Read all penns on page load
+function read_on_pageload(){
+   fetch_penns("http://localhost/Penner/api/penn/read.php")
+   .then(data => {display_penns(data)})
 }
 
 
+// Fetch url penns query
+async function fetch_penns_by_query() {
+   // Get selected option value
+   const select = document.getElementById('query');
+   var value = select.options[select.selectedIndex].value;
+   console.log(value);
+   // Get users input
+   const text = document.getElementById('search-bar-text').value;
+   console.log(text);
+   
+   // query users option
+   if(value == 'name'){
+      fetch_penns("http://" + url + "/Penner/api/penn/read_by_name.php?name=" + text)
+      .then(data => {display_penns(data)})
+   }
+   else if(value == 'color'){
+      fetch_penns("http://" + url + "/Penner/api/penn/read_by_color.php?color=" + text)
+      .then(data => {display_penns(data)})
+   }
+   else if(value == 'firma'){
+      fetch_penns("http://" + url + "/Penner/api/penn/read_by_firma.php?firma=" + text)
+      .then(data => {display_penns(data)})
 
-function display_penns() {
+   }
+   else{console.log("No option selected")}
+}
+
+
+// Fetch url penns
+async function fetch_penns(url) {
+   const link = url;
+   const login = 'Svein';
+   const password = '12345';
+   const response = await fetch(link, {
+   headers:{
+      "Authorization": `Basic ${btoa(`${login}:${password}`)}`,
+      'Content-Type': 'application/json'
+   },
+   });
+
+   return response.json();
+}
+
+
+// Display fetched data
+function display_penns(data) {
+   // Clear old penns display if it exists
+   const element_old = document.getElementById('penns-display');
+   if(element_old){element_old.remove()}
+
+   // Create new div for the penns display
    const element = document.createElement('div');
-   // Fetch penns
-   fetch_penns()
-   // Display data to the user
-   .then(data => {
+   element.setAttribute('id', 'penns-display');
+
+   // If no penns display message
+   if(data.message){
+      element.appendChild(document.createTextNode(data.message))
+   }
+   else{
+      // Display data to the user
       for(const res of data.data){
-         console.log(res.name);
          element.appendChild(document.createElement("br"))
          element.appendChild(document.createTextNode("Name: "))
          element.appendChild(document.createTextNode(res.name))
-     
+      
          element.appendChild(document.createElement("br"))
          element.appendChild(document.createTextNode("Type: "))
          element.appendChild(document.createTextNode(res.type))
-     
+      
          element.appendChild(document.createElement("br"))
          element.appendChild(document.createTextNode("Color: "))
          element.appendChild(document.createTextNode(res.color))
-     
+      
          element.appendChild(document.createElement("br"))
          element.appendChild(document.createTextNode("Firma: "))
          element.appendChild(document.createTextNode(res.firma_name))
-     
+         
+         // Append image if it exists
+         if(res.image){
+            let imgele = document.createElement('img')
+            imgele.setAttribute('src', 'http://localhost/Penner/images/' + res.image)
+            imgele.setAttribute("width", "150");
+   
+            element.appendChild(document.createElement("br"))         
+            element.appendChild(imgele)
+         }
+
          element.appendChild(document.createElement("br"))
          element.appendChild(document.createElement("br"))
       }
-   })
 
+   }
+
+   // Append created elements to the penns div
    document.getElementById("penns").append(element);
 }
